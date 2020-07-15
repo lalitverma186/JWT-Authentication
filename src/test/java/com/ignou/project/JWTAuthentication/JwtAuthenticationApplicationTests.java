945d623b95817f190e40a2e4bfdb20fc1af5f62e
lalitverma186 <lalit.verma44@gmail.com>
2020-07-15 22:27:05 +0530

    Project code for IGNOU

---
+++ src/test/java/com/ignou/project/JWTAuthentication/JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignou.project.JWTAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
